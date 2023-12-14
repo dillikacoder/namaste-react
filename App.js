7e58b13240f82f347b95bafcaf1dc6dcca989ad7
DilliKaCoder - Vikas Kardam <145770148+dillikacoder@users.noreply.github.com>
2023-12-14 17:58:08 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div", {
+    id:"parent",
+    xyz:"test"
+}, "Hello React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+dsfs
